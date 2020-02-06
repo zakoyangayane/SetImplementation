995c31e0317f2bdf697e5b31d562132aa6341c44
zakoyangayane <gayanezakoyan@gmail.com>
2020-02-07 01:39:21 +0400

    added Set interface

---
+++ src/com/homework/set/Set.java
@@ -0,0 +1,19 @@
+package com.homework.set;
+
+/**
+ * The Set interface is created for making set of any kind of objects, to implement this interface
+ * in other types and Set and overriding these methods giving them their own functionality.
+ *
+ * @param <T>
+ * @author Zakoyan Gayane
+ * @since 2020-02-07
+ */
+public interface Set<T> {
+    boolean add(T element);
+
+    boolean remove(T element);
+
+    boolean contains(T element);
+
+    int getSize(Set<T> element);
+}
