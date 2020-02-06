6e3adb62161c70ffafe964932eae94a2f3cbac50
zakoyangayane <gayanezakoyan@gmail.com>
2020-02-07 01:54:40 +0400

    added implementations of MyLinkedList and TreeSet

---
+++ src/com/homework/set/MyLinkedList.java
@@ -0,0 +1,106 @@
+package com.homework.set;
+
+/**
+ * MyLinkedList is an implementation in this application for helping creating the TreeSet
+ * type of Set. It's not the current implementation of the LinkedList.
+ * Here the elements of any type are kept in sorted order. And never a duplicate
+ * can be requested to be added here, cause the TreeSet from where this type of objects is created,
+ * doesn't call here if an element is already existing in the list here.
+ * Any type of object assigned to these class should be extending Comparable interface.
+ *
+ * @param <T>
+ * @author Zakoyan Gayane
+ * @since 2020-02-07
+ */
+public class MyLinkedList<T extends Comparable<T>> {
+    private Node<T> first;
+    private Node<T> last;
+    int listSize;
+
+    static class Node<T extends Comparable<T>> {
+        public Node<T> previous;
+        public T current;
+        public Node<T> next;
+
+        public Node(Node<T> previous, T current, Node<T> next) {
+            this.previous = previous;
+            this.current = current;
+            this.next = next;
+        }
+    }
+
+    /*add an element to the list, to its correct position*/
+    public void add(T element) {
+        Node<T> node = new Node<T>(null, element, null);
+        if (listSize == 0) {
+            first = last = node;
+        } else {
+            for (Node<T> n = this.first; n != null; n = n.next) {
+                if (n.current.compareTo(element) > 0) {
+                    Node<T> swap = null;
+                    if (n.previous != null) {
+                        swap = n.previous;
+                    }
+                    n.previous = node;
+                    node.next = n;
+
+                    if (swap != null) {
+                        swap.next = node;
+                        node.previous = swap;
+                    } else {
+                        first = node;
+                    }
+                    break;
+                }
+                if (n.current.compareTo(element) < 0) {
+                    if (n.next == null) {
+                        node.previous = n;
+                        n.next = node;
+                        last = node;
+                        break;
+                    }
+                }
+            }
+        }
+        listSize++;
+    }
+
+    /*remove an element from a list*/
+    public void remove(T element) {
+        for (Node<T> n = this.first; n != null; n = n.next) {
+            if (n.current.equals(element)) {
+                if (n.next == null) {
+                    n.previous.next = null;
+                } else if (n.previous == null) {
+                    n.next.previous = null;
+                } else {
+                    n.previous.next = n.next;
+                    n.next.previous = n.previous;
+                }
+                listSize--;
+            }
+        }
+    }
+
+    /*returns the number of elements in this list*/
+    public int size() {
+        return listSize;
+    }
+
+    /*prints the list*/
+    public void printInfo(MyLinkedList<T> myList) {
+        for (Node<T> n = myList.first; n != null; n = n.next) {
+            System.out.println(n.current);
+        }
+    }
+
+    /*checks whether the list contains the current element*/
+    public boolean contains(T t) {
+        for (Node<T> n = this.first; n != null; n = n.next) {
+            if (n.current.equals(t)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/com/homework/set/TreeSet.java
@@ -0,0 +1,75 @@
+package com.homework.set;
+
+/**
+ * The TreeSet implementation is done using the MyLinkedList class and the Set interface.
+ * Any object assigned to this class should extend the Comparable interface.
+ * All objects in this type of set are kept in sorted order, where no duplicates are allowed,
+ * and no null elements allowed here.
+ *
+ * @param <T>
+ * @author Zakoyan Gayane
+ * @since 2020-02-07
+ */
+public class TreeSet<T extends Comparable<T>> implements Set<T> {
+    public MyLinkedList<T> elements;
+
+    public TreeSet() {
+        elements = new MyLinkedList<>();
+    }
+
+    /*adds the element to the set if it is not already present there and if it's not equal to null*/
+    @Override
+    public boolean add(T element) {
+        if (element == null) {
+            throw new NullPointerException("TreeSet doesn't allow null element");
+        }
+        if (!this.contains(element)) {
+            this.elements.add(element);
+            return true;
+        }
+        return false;
+    }
+
+    /*removes the current element from the set if it is present there*/
+    @Override
+    public boolean remove(T element) {
+        if (element == null) {
+            throw new NullPointerException("TreeSet doesn't contain null element");
+        }
+        if (this.contains(element)) {
+            this.elements.remove(element);
+            return true;
+        }
+        return false;
+    }
+
+    /*checks whether the current element is included in the set or not*/
+    @Override
+    public boolean contains(T element) {
+        if (element == null) {
+            throw new NullPointerException("TreeSet doesn't contain null element");
+        }
+        return this.elements.contains(element);
+    }
+
+    /*gives the size of the set*/
+    @Override
+    public int getSize(Set<T> element) {
+        if (this.elements == null) {
+            throw new NullPointerException("The TreeSet is null");
+        }
+        return this.elements.size();
+    }
+
+    /*prints the elements of the list*/
+    public void printElements() {
+        this.elements.printInfo(this.elements);
+    }
+
+    @Override
+    public String toString() {
+        return "TreeSet{" +
+                "elements=" + elements +
+                '}';
+    }
+}
